3cf02264f04cfa239df7707e498915ca7f3e7b69
aakash <aakash121296@google.com>
2017-06-10 20:21:07 +0530

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,35 @@ restServices.use(bodyParser.urlencoded({
 restServices.use(bodyParser.json());
 
 restServices.post('/echo', function(req , res ){
-	var speech =  "wonder woman";
+
+	var url = 'http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1';
+	
+	request(url, function(error, response, html){
+	if(!error){
+	var $ = cheerio.load(html);
+	
+	var title;
+	var tit = [];
+        var rating;
+	var n = 0;
+	
+	$('.rating_txt').filter(function(){
+	var data = $(this);
+	rating = data.children().first().text();
+	if(rating > 10)
+	{
+	n++;console.log(n);}
+            })
+	$('.overview-top').filter(function(){
+	var data = $(this);
+	title = data.children().first().text();
+	tit.push(title);
+	})
+
+        }
+    })	
+
+	var speech =  tit[n];
 	return res.json({
 		speech: speech,
 		displayText: speech,
