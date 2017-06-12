ccca36f94943ff80f10943a35c6a7d7a2af60ea6
aakash <aakash121296@google.com>
2017-06-12 14:18:02 +0530

    top one

--- server.js
+++ server.js
@@ -11,40 +11,29 @@ const restServices = express();
 restServices.use(bodyParser.urlencoded({
 	extended: true
 	}));
+var title;
+var tit = [];
+var n;
 
 restServices.use(bodyParser.json());
-
-var url = 'http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1';
+restServices.post('/echo', function(req , res ){
+	var url = 'http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1';
 	
 	request(url, function(error, response, html){
 	if(!error){
 	var $ = cheerio.load(html);
 	
-	var title;
-	var tit = [];
-        var rating;
-	var n = 0;
-	
-	$('.rating_txt').filter(function(){
-	var data = $(this);
-	rating = data.children().first().text();
-	if(rating > 10)
-	{
-	n++;console.log(n);}
-            })
 	$('.overview-top').filter(function(){
 	var data = $(this);
 	title = data.children().first().text();
 	tit.push(title);
 	})
-
-        }console.log(tit[n]);
+	n = tit.length;
+        }
+	console.log(tit[n-10]);
     })	
 
-
-restServices.post('/echo', function(req , res ){
-
-	var speech =  JSON.stringify(tit[n]);
+	var speech =  "Wonder Woman";
 	return res.json({
 		speech: speech,
 		displayText: speech,
