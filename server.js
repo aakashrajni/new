642398b0b3541edc81e1d8162f2f463d899bf3c4
aakash <aakash121296@google.com>
2017-06-20 12:52:32 +0530

    little change

--- server.js
+++ server.js
@@ -11,14 +11,29 @@ const restServices = express();
 restServices.use(bodyParser.urlencoded({
 	extended: true
 	}));
-var title;
-var tit = [];
-var n;
+var title,ttitle;
+var tit = [];var ttit = [];
+var n,t;
+var arrt = [1,2,3,4,5];
 var arr = [10,9,8,7,6];
 restServices.use(bodyParser.json());
+
+	var url = 'https://www.filmipop.com/chennai/movie/moviesearch?sp=bycinema.PVR%20-%20Ampa%20Sky%20Walk%20Mall,%20Aminjikerai__bylanguage.Tamil__byrunning.1';
+	request(url, function(error, response, html){
+	if(!error){
+	var $ = cheerio.load(html);
+	$('.movie-title').filter(function(){
+	var data = $(this);
+	ttitle = data.children().first().text();
+	ttit.push(ttitle);
+	});}
+	});
+
 restServices.post('/echo', function(req , res ){
+
+	if(req.body.result.parameters.movietell == best)
+	{
 	var url = 'http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1';	
-	console.log(req.body.result.parameters.movietell);
 	request(url, function(error, response, html){
 	if(!error){
 	var $ = cheerio.load(html);
@@ -39,6 +54,27 @@ restServices.post('/echo', function(req , res ){
 		displayText: speech,
 		source: 'webhook-echo-sample'
 	});
+	}
+	if(req.body.result.parameters.movietell == best)
+	{
+	var url = 'https://www.filmipop.com/chennai/movie/moviesearch?sp=bycinema.PVR%20-%20Ampa%20Sky%20Walk%20Mall,%20Aminjikerai__bylanguage.Tamil__byrunning.1';
+	request(url, function(error, response, html){
+	if(!error){
+	var $ = cheerio.load(html);
+	$('.movie-title').filter(function(){
+	var data = $(this);
+	ttitle = data.children().first().text();
+	ttit.push(ttitle);
+	});}
+	});
+	var rand = arrt[Math.floor(Math.random() * arrt.length)];
+	var speech =  ttit[rand];
+	return res.json({
+		speech: speech,
+		displayText: speech,
+		source: 'webhook-echo-sample'
+	});
+	}
 });
 
 restServices.listen((process.env.PORT || 5000), function() {
