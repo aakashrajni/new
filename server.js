ec2545cd72ab4adcc5c9fb88147fb20acc489262
aakash <aakash121296@google.com>
2017-06-20 11:41:26 +0530

    little change

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ var n;
 var arr = [10,9,8,7,6];
 restServices.use(bodyParser.json());
 restServices.post('/echo', function(req , res ){
-	var url = 'http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1';
+	var url = 'http://www.imdb.com/movies-in-theaters/?ref_=nv_mv_inth_1';	
+	var jsonobj = JSON.parse(req.body);
+	console.log(jsonobj.result);
 	request(url, function(error, response, html){
 	if(!error){
 	var $ = cheerio.load(html);
